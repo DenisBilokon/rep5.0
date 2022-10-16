ccac485cf74fa3e50e4f629db3031d2f5db1e650
DenisBilokon <106659593+DenisBilokon@users.noreply.github.com>
2022-10-16 16:03:36 +0300

    Add files via upload

---
+++ 5.2.cs
@@ -0,0 +1,133 @@
+﻿
+class ExcelentPupil : Pupil
+{
+    public override void Study()
+    {
+        Console.WriteLine("Вiдмiнник: Вчиться - вiдмiнно");
+    }
+    public override void Read()
+    {
+        Console.WriteLine("Вiдмiнник: Читає - вiдмiнно");
+    }
+    public override void Write()
+    {
+        Console.WriteLine("Вiдмiнник: Пишe - вiдмiнно");
+    }
+    public override void Relax()
+    {
+        Console.WriteLine("Вiдмiнник: Вiдпочиває - вiдмiнно");
+    }
+
+}
+class GoodPupil : Pupil
+{
+    public override void Study()
+    {
+        Console.WriteLine("Гарний Учень: Вчиться - гарно");
+    }
+    public override void Read()
+    {
+        Console.WriteLine("Гарний Учень: Читає - гарно");
+    }
+    public override void Write()
+    {
+        Console.WriteLine("Гарний Учень: Пишe - гарно");
+    }
+    public override void Relax()
+    {
+        Console.WriteLine("Гарний Учень: Вiдпочиває - гарно");
+    }
+
+}
+class BadPupil : Pupil
+{
+    public override void Study()
+    {
+        Console.WriteLine("Поганий учень: Вчиться - погано ");
+    }
+    public override void Read()
+    {
+        Console.WriteLine("Поганий учень: Читає - погано");
+    }
+    public override void Write()
+    {
+        Console.WriteLine("Поганий учень: Пишe - погано");
+    }
+    public override void Relax()
+    {
+        Console.WriteLine("Поганий учень: Вiдпочиває - погано");
+    }
+
+}
+class ClassRoom
+{
+    Pupil[] arrPupil;
+
+    public ClassRoom(Pupil p1, Pupil p2, Pupil p3, Pupil p4)
+    {
+        arrPupil = new Pupil[] { p1, p2, p3, p4 };
+    }
+    public ClassRoom(Pupil p1, Pupil p2, Pupil p3)
+    {
+        arrPupil = new Pupil[] { p1, p2, p3 };
+    }
+    public ClassRoom(Pupil p1, Pupil p2)
+    {
+        arrPupil = new Pupil[] { p1, p2 };
+    }
+
+    public void PrintInfoStudy()
+    {
+        Console.WriteLine("Вчиться: ");
+        foreach (Pupil item in arrPupil)
+            item.Study();
+    }
+    public void PrintInfoRead()
+    {
+        Console.WriteLine("Читає: ");
+        foreach (Pupil item in arrPupil)
+            item.Read();
+    }
+    public void PrintInfoWrite()
+    {
+        Console.WriteLine("Пише: ");
+        foreach (Pupil item in arrPupil)
+            item.Write();
+    }
+    public void PrintInfoRelax()
+    {
+        Console.WriteLine("Вiдпочиває: ");
+        foreach (Pupil item in arrPupil)
+            item.Relax();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ClassRoom cRoom = new ClassRoom(new ExcelentPupil(), new GoodPupil(), new BadPupil(), new GoodPupil());
+
+        cRoom.PrintInfoRead();
+        cRoom.PrintInfoRelax();
+        cRoom.PrintInfoStudy();
+        cRoom.PrintInfoWrite();
+
+        Console.ReadKey();
+    }
+}
+
+class Pupil
+{
+    public virtual void Study()
+    { }
+
+    public virtual void Read()
+    { }
+
+    public virtual void Write()
+    { }
+
+    public virtual void Relax()
+    { }
+}
---
+++ 5.3.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleApplication238
+{
+    class Program
+    {
+        private const string PRO_LICENSE = "111";
+        private const string EXP_LICENSE = "222";
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введiть лiцензiйний ключ:");
+            var license = Console.ReadLine();
+
+
+            DocumentWorker worker;
+            switch (license)
+            {
+                case PRO_LICENSE: worker = new ProDocumentWorker(); break;
+                case EXP_LICENSE: worker = new ExpertDocumentWorker(); break;
+                default: worker = new DocumentWorker(); break;
+            }
+
+
+            while (true)
+            {
+                Console.WriteLine("Введiть команду (o/e/s/q): ");
+                switch (Console.ReadLine())
+                {
+                    case "o": worker.OpenDocument(); break;
+                    case "e": worker.EditDocument(); break;
+                    case "s": worker.SaveDocument(); break;
+                    case "q": return;
+                }
+            }
+        }
+    }
+
+    class DocumentWorker
+    {
+        public virtual void OpenDocument()
+        {
+            Console.WriteLine("Документ вiдкрито");
+        }
+
+        public virtual void EditDocument()
+        {
+            Console.WriteLine("Редагування документу доступно в версiї Про");
+        }
+
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine("зберiгання документу доступно в версiї Про");
+        }
+    }
+
+    class ProDocumentWorker : DocumentWorker
+    {
+        public override void EditDocument()
+        {
+            Console.WriteLine("Документ вiдреагован");
+        }
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ збережен в старому форматi");
+        }
+    }
+
+    class ExpertDocumentWorker : ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ збережен в новому форматi");
+        }
+    }
+}
+
